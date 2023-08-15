17977583610d3b12236bc1807b90662398d73fb8
Yelyzaveta Buhaichuk <bug.liza@gmail.com>
2023-08-15 15:51:25 +0300

    Commit 3: Add Ticket entity and CRUD service

---
+++ src/main/java/org/module_10/entity/Ticket.java
@@ -0,0 +1,29 @@
+package org.module_10.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.sql.Date;
+@Data
+@Entity
+@Table(name="Ticket")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Date createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id",  nullable = false)
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_planet_id", nullable = false)
+    private Planet from;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_planet_id", nullable = false)
+    private Planet to;
+
+}
---
+++ src/main/java/org/module_10/services/TicketCrudService.java
@@ -0,0 +1,104 @@
+package org.module_10.services;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.module_10.entity.Client;
+import org.module_10.entity.Planet;
+import org.module_10.entity.Ticket;
+import org.module_10.util.HibernateUtil;
+
+import java.util.List;
+
+public class TicketCrudService {
+    public long create(Ticket ticket) {
+        if(!validation(ticket)){
+            return -1;
+        }
+        Transaction transaction = null;
+        try (Session session = openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(ticket);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return ticket.getId();
+    }
+    public Ticket getById(String id) {
+        try (Session session = openSession()) {
+            Query<Ticket> query = session.createQuery("from Ticket where id = :id", Ticket.class);
+            query.setParameter("id", id);
+            return query.stream().findFirst().orElse(null);
+
+        }
+    }
+
+    public long update(Ticket ticket) {
+        if(!validation(ticket)){
+            return -1;
+        }
+        Transaction transaction = null;
+        try (Session session = openSession()) {
+            transaction = session.beginTransaction();
+            session.update(ticket);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return ticket.getId();
+    }
+    public List<Ticket> listAll() {
+        try (Session session = openSession()) {
+            return session.createQuery("from Ticket", Ticket.class).list();
+        }
+    }
+
+    public void deleteById(long id) {
+        Transaction transaction = null;
+        try (Session session = openSession()) {
+            transaction = session.beginTransaction();
+            Ticket ticket = session.get(Ticket.class, id);
+            if (ticket != null) {
+                session.remove(ticket);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    private Session openSession() {
+        return HibernateUtil
+                .getInstance()
+                .getSessionFactory()
+                .openSession();
+    }
+
+    private boolean validation(Ticket ticket) {
+        if (ticket == null || ticket.getClient() == null ||
+                ticket.getFrom() == null ||
+                ticket.getTo() == null) {
+            return false;
+        }
+        Client client = new ClientCrudService().getById(ticket.getClient().getId());
+        if (client == null)
+            return false;
+        Planet from = new PlanetCrudService().getById(ticket.getFrom().getId());
+        if (from == null)
+            return false;
+        Planet to = new PlanetCrudService().getById(ticket.getTo().getId());
+        if (to == null) {
+            return false;
+        }
+        return true;
+    }
+}
